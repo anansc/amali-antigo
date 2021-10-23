0607d384c02f02d27fa76e672e9fd61d401d1e9b
Ana <anahaydn3@gmail.com>
2021-10-23 02:58:12 -0300

    add escolasBean

---
+++ src/bean/EscolasBean.java
@@ -0,0 +1,72 @@
+package bean;
+
+public class EscolasBean {
+	
+	private int id;
+	private int user_id;
+	private int city_id;
+	private int uuid;
+	private String nome;
+	private String email;
+	private String cnpj;
+	private String telefone;
+	private String endereco;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public int getCity_id() {
+		return city_id;
+	}
+	public void setCity_id(int city_id) {
+		this.city_id = city_id;
+	}
+	public int getUuid() {
+		return uuid;
+	}
+	public void setUuid(int uuid) {
+		this.uuid = uuid;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+	
+	
+}
